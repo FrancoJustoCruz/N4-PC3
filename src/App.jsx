d7e17e9c96f3f8915a83cfba8ac246176f4f5372
justocruzfranco <justocruzfranco@gmail.com>
2024-05-16 19:00:57 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Destination from "./pages/Destination";
+import Crew from "./pages/Crew";
+import Technology from "./pages/Technology";
+import fondo from './assets/fondo-home-desktop.jpg'
+import './index.css'
+
+const App = () => {
+  return (
+    <main className="main-container ">
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Home />} />
+          <Route path="destination" element={<Destination />}></Route>
+          <Route path="crew" element={<Crew />} />
+          <Route path="technology" element={<Technology />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
